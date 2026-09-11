172619c6c80af01527469af83ee7bd0d6d3118b8
Markus Ang <maokooss@gmail.com>
2026-09-11 15:25:10 -0400

    lolwhoops2

--- packages/create-mvmnt-plugin/bin/create-mvmnt-plugin.mjs
+++ packages/create-mvmnt-plugin/bin/create-mvmnt-plugin.mjs
@@ -274,7 +274,7 @@ export function createPromptQuestions(options) {
             type: options.element ? null : 'text',
             name: 'element',
             message: 'Element type',
-            initial: 'my-plugin-element',
+            initial: (_previous, values) => (values.name ?? options.name ?? 'com.example.my-plugin').split('.').at(-1),
             validate: (value) =>
                 validateElementType(value) ? true : 'Use a kebab-case name, such as my-plugin-element.',
         },
